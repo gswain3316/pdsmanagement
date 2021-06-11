d7aecf62a4d8bc3c07bf535bcadf7a8b44c292d6
gswain3316 <gswain3316@live.com>
2021-06-11 17:09:41 +0530

    intial project structure created

---
+++ src/main/java/com/zerohunger/pdsmanagement/PdsmanagementLocalApplication.java
@@ -0,0 +1,13 @@
+package com.zerohunger.pdsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdsmanagementLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdsmanagementLocalApplication.class, args);
+	}
+
+}
